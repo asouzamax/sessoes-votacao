c2c4bcb117bb647a3caf1f33126b4e90283bfddd
asouzamax <asouzamax@gmail.com>
2022-07-04 15:32:53 -0300

    Delete target directory

--- target/classes/db.migration/V001__tabela-associado-criacao.sql
+++
@@ -1,6 +0,0 @@
-create table associado (
-	id varchar(64) not null default random_uuid(),
-    nome varchar(128) not null,
-    cadastro TIMESTAMP WITH TIME ZONE not null default now(),
-    primary key (id)
-);
